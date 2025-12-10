8d8f50b52b92ab8dc37820b11d473d62722f3b1f
AzaDork <antoni.caulier@gmail.com>
2025-12-10 12:44:42 +0100

    new path file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import ArtistsPage from './pages/Artists';
+import ArtistsPage from './pages/ArtistsPage';
 import ArtistDetailPage from './pages/ArtistDetailPage';
 import './index.css';
 
