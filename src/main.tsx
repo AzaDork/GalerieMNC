f39561b20aac46f5bbd97059c657c1d04148124b
AzaDork <antoni.caulier@gmail.com>
2025-12-10 14:03:33 +0100

    redeploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,15 @@
+// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import ArtistsPage from './pages/ArtistsPage';
-import ArtistDetailPage from './pages/ArtistDetailPage';
+import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* Page d’accueil */}
-        <Route path="/" element={<App />} />
+const container = document.getElementById('root');
 
-        {/* Liste des artistes */}
-        <Route path="/artists" element={<ArtistsPage />} />
-
-        {/* Page individuelle artiste : /artistes/anne-goujaud */}
-        <Route path="/artistes/:slug" element={<ArtistDetailPage />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
+if (container) {
+  createRoot(container).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
