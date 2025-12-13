278782250ebaa48597613803ba10512520d6d614
AzaDork <antoni.caulier@gmail.com>
2025-12-13 17:39:51 +0100

    page d'accueil

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -41,7 +41,8 @@ const Hero: React.FC = () => {
               Galerie MNC
             </h1>
             <p className="text-xl md:text-2xl font-light mb-8 opacity-0 animate-fade-in animation-delay-200">
-              Art contemporain & Encadrement sur mesure
+              Art contemporain
+              Atelier d'encadrement
             </p>
             <Link 
               to="/artistes"
