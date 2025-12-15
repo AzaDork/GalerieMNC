4a584b833ddfaf6ff57dd74f0955cabd4cf055b2
AzaDork <antoni.caulier@gmail.com>
2025-12-15 17:25:54 +0100

    improve SEO

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
 const container = document.getElementById('root');
 
-if (container) {
-  createRoot(container).render(
-    <StrictMode>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </StrictMode>
-  );
+if (!container) {
+  throw new Error('Root container missing in index.html');
 }
+
+createRoot(container).render(
+  <StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </StrictMode>
+);
