d3679ddc1a1d86b9c80c413f1c6af6ea0d0ae1cf
AzaDork <antoni.caulier@gmail.com>
2025-12-23 18:47:43 +0100

    encadrement description

--- src/components/FraimingDescription.tsx
+++ src/components/FraimingDescription.tsx
@@ -6,7 +6,10 @@ const FraimingDescritpion: React.FC = () => {
       <div className="container mx-auto px-4 md:px-8">
         <div className="max-w-6xl mx-auto text-center">
           <p className="text-lg md:text-1xl text-gray-800 leading-relaxed">
-            À COMPLÉTER !!!!
+            Notre atelier d’encadrement vous propose un très large choix de baguettes pour les œuvres choisies à la galerie aussi bien que pour celles que vous nous déposerez.
+            Pour chaque oeuvre à encadrer, nous vous proposerons plusieurs choix d’encadrement et plusieurs devis. 
+            
+            Nous utilisons uniquement des matériaux neutres, museum, etc...
           </p>
         </div>
       </div>
