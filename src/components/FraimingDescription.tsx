f4cbdc46a7fc2d72381067a05545c05f73f5534c
AzaDork <antoni.caulier@gmail.com>
2025-12-23 18:50:29 +0100

    encadrement description

--- src/components/FraimingDescription.tsx
+++ src/components/FraimingDescription.tsx
@@ -5,12 +5,17 @@ const FraimingDescritpion: React.FC = () => {
     <section className="py-24 bg-white">
       <div className="container mx-auto px-4 md:px-8">
         <div className="max-w-6xl mx-auto text-center">
-          <p className="text-lg md:text-1xl text-gray-800 leading-relaxed">
-            Notre atelier d’encadrement vous propose un très large choix de baguettes pour les œuvres choisies à la galerie aussi bien que pour celles que vous nous déposerez.
-            Pour chaque oeuvre à encadrer, nous vous proposerons plusieurs choix d’encadrement et plusieurs devis. 
-            
-            Nous utilisons uniquement des matériaux neutres, museum, etc...
-          </p>
+          <div className="text-lg md:text-1xl text-gray-800 leading-relaxed space-y-6">
+            <p>
+              Notre atelier d’encadrement vous propose un très large choix de baguettes pour les œuvres choisies à la galerie aussi bien que pour celles que vous nous déposerez.
+            </p>
+            <p>
+              Pour chaque oeuvre à encadrer, nous vous proposerons plusieurs choix d’encadrement et plusieurs devis.
+            </p>
+            <p>
+              Nous utilisons uniquement des matériaux neutres, museum, etc...
+            </p>
+          </div>
         </div>
       </div>
     </section>
